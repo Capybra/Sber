04dc386a687e4a34d1becbf4b507510313e98277
Cucumber778 <zabelinsergei@bk.ru>
2023-11-18 17:25:25 +0900

    repair

--- users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profile
-
-class ProfileAdmin(admin.ModelAdmin):
-    list_display =['user', 'name', 'phone', 'rating', 'image', 'created_at', 'updated_at']
-    list_filter = ['rating', 'created_at', 'updated_at']
-    list_editable = []
-
-admin.site.register(Profile, ProfileAdmin)
